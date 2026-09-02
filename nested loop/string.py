82aca450a8219dda2e671e9c683631460743c792
Yash <suryawanshilalit2005@gmail.com>
2026-09-03 03:48:56 +0530

    Add string traversal examples

--- nested loop/string.py
+++ nested loop/string.py
@@ -9,3 +9,22 @@ for i in range(len(s)):
 for i in range(len(s)):
     if s[i] == "T":
         print("the given charecter is present at ",i,"index")
+
+        #for in banana we  are checking the reapeating a and printing the index of a
+        s="BANANA"
+c=0
+for i in range (len(s)):
+    if s[i] =="A":
+        c=i
+        print(c)
+    
+
+    #for print after first char is found in the string
+    s="BANANA"
+
+for i in range (len(s)):
+    if s[i] =="A":
+        break
+
+print(i)
+  
