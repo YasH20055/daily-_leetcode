d5634bc05e8f13bcc61ca63da79ee3c24b07b3af
Yash <suryawanshilalit2005@gmail.com>
2026-09-02 23:28:21 +0530

    Add string character and vowel checks

--- nested loop/string.py
+++ nested loop/string.py
@@ -2,3 +2,10 @@ s = "DOG"
 
 for i in range(len(s)):
     print(i, s[i])
+
+    # for printing the index and character at that index in the string 
+    s = "PYTHON"
+
+for i in range(len(s)):
+    if s[i] == "T":
+        print("the given charecter is present at ",i,"index")
