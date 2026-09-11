8d3c225d1b3a7086f51a86d45305edfd49ed4eeb
Yash <suryawanshilalit2005@gmail.com>
2026-09-12 00:28:28 +0530

    Add argument and mistake practice scripts

---
+++ mist.py
@@ -0,0 +1,5 @@
+def employee_info(name, salary, department):
+    print("name:", name)
+    print("salary:", salary)
+    print("department:", department)
+employee_info(department="robotics",salary=4000,name="yash")
